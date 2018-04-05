98eff10148552a20a9df207d512b8d770547ca0c
pertbanking <joshua.m.petrin@vanderbilt.edu>
2018-04-05 01:57:24 -0500

    Added comments. Several issues; will fix tomorrow.

--- UncleMorris/UncleMorris.ino
+++ UncleMorris/UncleMorris.ino
@@ -11,13 +11,22 @@ char morseCodeKey [36][7] = {"01 ","1000 ","1010 ","100 ","0 ","0010 ", // abcde
                           "01111 ","00111 ","00011 ","00001 ","00000 ", // 01234
                           "10000 ","11000 ","11100 ","11110 ","11111 "};// 56789
 
+// Danny,
+// Do NOT indent with tabs. Ever. If you do it again, I will do something mean to you in return.
+// Also, make sure your code is readable. If it looks semantically different than what I have written 
+//  below, it is probably difficult to understand to most people. Please read Google's Style Guides
+//  for C++; your previous code made some pretty bad sins.
+// https://google.github.io/styleguide/cppguide.html
+
+// But thanks anyway for doing this. 
+// - Josh
 
 int thermo = A0;
 int photo = A3;
 int thermoTarget = 1234;
 int photoTarget = 4321;
 char message[] = "abcdefghijklmnopqrstuvwxyz0123456789"; // MUST BE lowercase alphanumeric
- //36 alphanumeric numbers, 0 for dot,1 for dash letters then numbers
+ //36 alphanumeric numbers, 0 for dot, 1 for dash letters then numbers
 int ledPin = 13;
 int unitTime = 1000;  //in ms
 int bufferTime = 50;  //in ms
@@ -25,7 +34,7 @@ bool found = false;
 
  
 
-int letterCount = sizeof(message);
+size_t letterCount = sizeof(message);
 int letterIndex = 0;
 
 void setup()
@@ -43,16 +52,18 @@ void loop()
 }
 
 bool checkSensors() {
-  //boolean zen ;)
-	return (analogRead(thermo) == thermo) && (analogRead(photo) == photoTarget);
+  // NO. DO NOT DO THIS. Will only read true if the analog read is EXACTLY the designated target value.
+  // Set a true result for a range of values instead.
+  return (analogRead(thermo) == thermoTarget) && (analogRead(photo) == photoTarget);
 }
 
 void displayNextLetter() {
-  
   int keyIndex;
   char morseChar = message[letterIndex];
   Serial.print(morseChar);
-  
+
+  // WTF are these values???? Are they ASCII? I can't tell; you didn't even comment your code
+  // Compare your chars to other chars; e.g. morseChar == ' '; or morseChar < 'a';.
   if (morseChar == 32) {
     wordSpace();
   } else if (morseChar < 58) {
@@ -76,23 +87,35 @@ void displayNextLetter() {
   letterSpace();
 }
 
+
+// I get the feeling there is a cleaner way to code these functions...
+// Maybe have a delayStep() function and then have a counter for the 
+//  number of times delayStep() needs to be called for the current state?
+// int delayCount;
+// dot  -> delayCount = 1;
+// dash -> delayCount = 3;
+// charSpace -> delaycount = 1;
+// wordSpace -> delayCount = 3;
+// etc.
+// btw, there are standards for how long these delays are supposed to be. 
 void interSpace(){
-	delay(unitTime);
+  delay(unitTime);
 }
 
 void letterSpace(){
-	delay(unitTime*3);
+  delay(unitTime*3);
 }
 
 void wordSpace(){
-	delay(unitTime*4);
+  delay(unitTime*4);
 }
 
 void dash(){
+  // What is this print() supposed to do
   Serial.print(3);
   for(int i = 0; i < 3; ++i) {
-  	digitalWrite(ledPin, HIGH);
-  	delay(unitTime - bufferTime);
+    digitalWrite(ledPin, HIGH);
+    delay(unitTime - bufferTime);
     digitalWrite(ledPin, LOW);
     delay(bufferTime);
   }
